ef284294b99aa0a924c54cf2636bdd5f270566a6
cammoraton <nick_cammorato@terc.edu>
2014-05-06 20:39:52 -0400

    spacing

--- plugins/guests/redhat/cap/interfaces_list.rb
+++ plugins/guests/redhat/cap/interfaces_list.rb
@@ -13,7 +13,7 @@ module VagrantPlugins
 
           # Only retrieve the list if we're in EL7+
           # Biosdevname was unreliably present in el6 and an ethX assumption was how it was done before
-          if version.to_i> 6
+          if version.to_i > 6
             machine.communicate.sudo("biosdevname -d | grep Kernel | cut -f2 -d: | sed -e 's/ //;'") do |_, result|
               interface_names = result.split("\n")
             end
