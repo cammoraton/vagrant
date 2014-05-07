c7e538060936915cdbea174d771ff3fe73f0d224
cammoraton <nick_cammorato@terc.edu>
2014-05-06 20:38:10 -0400

    Remove assumption of biosdevname is el6.  Return blank array to default back to ethX assumption in pre-EL7

--- plugins/guests/redhat/cap/interfaces_list.rb
+++ plugins/guests/redhat/cap/interfaces_list.rb
@@ -11,16 +11,11 @@ module VagrantPlugins
           
           interface_names = Array.new
 
-          # In theory this would work with even older versions as dmesg has been relatively static for a long time
-          if version.to_i < 6
-            machine.communicate.sudo("dmesg | cut -f2 -d: | sed -e 's/^\ //' | sed -e 's/\ .*$//' | grep eth") do |_, result|
-              # It has two results ? - Quick hack to compensate
-              interface_names = result.split("\n").uniq.sort if interface_names.empty?
-            end
-          else
+          # Only retrieve the list if we're in EL7+
+          # Biosdevname was unreliably present in el6 and an ethX assumption was how it was done before
+          if version.to_i> 6
             machine.communicate.sudo("biosdevname -d | grep Kernel | cut -f2 -d: | sed -e 's/ //;'") do |_, result|
-              # The previous had two results.  This one never has.  Do the same check for now.
-              interface_names = result.split("\n") if interface_names.empty?
+              interface_names = result.split("\n")
             end
           end
           
